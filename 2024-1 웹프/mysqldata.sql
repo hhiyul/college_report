5f3109092ac160caa87daebca8b76806766ebaac
hhiyul <162940405+hhiyul@users.noreply.github.com>
2024-06-12 11:01:44 +0900

    Create mysqldata.sql

---
+++ 2024-1 웹프/mysqldata.sql
@@ -0,0 +1,23 @@
+-- 'post' 테이블 생성
+CREATE TABLE post (
+    num INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(50) NOT NULL,
+    writer VARCHAR(50) NOT NULL,
+    content TEXT NOT NULL,
+    reg_date DATETIME NOT NULL,
+    pass VARCHAR(50) NOT NULL,
+    view INT DEFAULT 0,
+    PRIMARY KEY (num)
+);
+
+-- 'comments' 테이블 생성
+CREATE TABLE comments (
+    comment_id INT NOT NULL AUTO_INCREMENT,
+    post_num INT NOT NULL,
+    writer VARCHAR(255),
+    content TEXT,
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    comment_pass VARCHAR(50) NOT NULL,
+    PRIMARY KEY (comment_id),
+    FOREIGN KEY (post_num) REFERENCES post(num)
+);
